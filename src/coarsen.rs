f747043508cab43d499ce6180ec5da51146a2891
Russel Arbore <russel.jma@gmail.com>
2024-12-03 14:50:51 -0600

    coarsening

--- src/coarsen.rs
+++ src/coarsen.rs
@@ -1,16 +1,57 @@
+use std::collections::BinaryHeap;
+
+use bitvec::prelude::*;
+use ordered_float::OrderedFloat;
+
 use crate::bipartite::*;
 
 pub(crate) fn coarsen(h: &mut Bipartite) {
-    todo!()
+    let c_max = S * h.total_capacity() / T as f32;
+
+    let mut pq = BinaryHeap::new();
+    for u in 0..h.v.len() as Index {
+        if h.c[u as usize] > c_max {
+            continue;
+        }
+
+        let v = h
+            .incident_pins(u)
+            .max_by_key(|v| OrderedFloat(rate(h, u, *v)))
+            .unwrap();
+        pq.push((OrderedFloat(rate(h, u, v)), (u, v)));
+    }
+    let mut removed = bitvec![usize, Lsb0; 0; h.v.len()];
+    let mut invalid = bitvec![usize, Lsb0; 0; h.v.len()];
+
+    while h.v.len() >= T {
+        let Some((_, (u, v))) = pq.pop() else {
+            continue;
+        };
+
+        if removed[u as usize] {
+            continue;
+        } else if invalid[u as usize] {
+            let v = h
+                .incident_pins(u)
+                .max_by_key(|v| OrderedFloat(rate(h, u, *v)))
+                .unwrap();
+            pq.push((OrderedFloat(rate(h, u, v)), (u, v)));
+            invalid.set(u as usize, false);
+            continue;
+        }
+
+        h.contract(u, v);
+        removed.set(v as usize, true);
+
+        for n in h.incident_pins(u) {
+            invalid.set(n as usize, true);
+        }
+    }
 }
 
 /// Constants from Section 5 from Schlag '2015.
 const T: usize = 320;
 const S: f32 = 3.25;
-
-fn needs_coarsening(h: &Bipartite) -> bool {
-    todo!()
-}
 
 fn rate(h: &Bipartite, u: Index, v: Index) -> f32 {
     let inv_c = 1.0 / (h.c[u as usize] * h.c[v as usize]);
