42818779ed7a8dda53f6544a398b5dba0536fb4b
Russel Arbore <russel.jma@gmail.com>
2024-12-04 14:57:52 -0600

    not going to uncoarsen for now

--- src/uncoarsen.rs
+++ src/uncoarsen.rs
@@ -1,20 +1,36 @@
 use std::collections::BinaryHeap;
 
+use bitvec::prelude::*;
 use ordered_float::OrderedFloat;
 
 use crate::bipartite::*;
 
-pub(crate) fn uncoarsen(h: &mut Bipartite, mementos: Vec<Memento>, partition: &mut Bipartition) {
+pub(crate) fn uncoarsen(
+    h: &mut Bipartite,
+    size_constraint: f32,
+    mementos: Vec<Memento>,
+    partition: &mut Bipartition,
+) {
     let mut b1_pq = BinaryHeap::new();
     let mut b2_pq = BinaryHeap::new();
-    let mut b1_enabled = false;
-    let mut b2_enabled = false;
+    let mut b1_total = 0.0;
+    let mut b2_total = 0.0;
+    let mut active = bitvec![usize, Lsb0; 0; h.pin_index_space_size()];
+    let mut marked = bitvec![usize, Lsb0; 0; h.pin_index_space_size()];
+    for v in h.pins() {
+        if partition[v as usize] {
+            b1_total += h.capacity(v);
+        } else {
+            b2_total += h.capacity(v);
+        }
+    }
 
     let activate = |v: Index,
                     h: &Bipartite,
                     partition: &Bipartition,
                     b1_pq: &mut BinaryHeap<(OrderedFloat<f32>, Index)>,
-                    b2_pq: &mut BinaryHeap<(OrderedFloat<f32>, Index)>| {
+                    b2_pq: &mut BinaryHeap<(OrderedFloat<f32>, Index)>,
+                    active: &mut BitVec| {
         let b_v = partition[v as usize];
         let g = OrderedFloat(gain(h, v, !b_v, partition));
         if b_v {
@@ -22,6 +38,7 @@ pub(crate) fn uncoarsen(h: &mut Bipartite, mementos: Vec<Memento>, partition: &m
         } else {
             b1_pq.push((g, v));
         }
+        active.set(v as usize, true);
     };
 
     for memento in mementos.into_iter().rev() {
@@ -34,13 +51,13 @@ pub(crate) fn uncoarsen(h: &mut Bipartite, mementos: Vec<Memento>, partition: &m
             .incident_pins(u)
             .any(|p| partition[p as usize] != partition[u as usize]);
         if border_u {
-            activate(u, h, partition, &mut b1_pq, &mut b2_pq);
+            activate(u, h, partition, &mut b1_pq, &mut b2_pq, &mut active);
         }
         let border_v = h
             .incident_pins(v)
             .any(|p| partition[p as usize] != partition[v as usize]);
         if border_v {
-            activate(v, h, partition, &mut b1_pq, &mut b2_pq);
+            activate(v, h, partition, &mut b1_pq, &mut b2_pq, &mut active);
         }
         if !border_u && !border_v {
             continue;
